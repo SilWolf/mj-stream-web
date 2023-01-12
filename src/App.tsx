ffe6f988a00cd82646c482c829565577869c468f
SilWolf <silwolf1121@gmail.com>
2023-01-12 17:38:12 +0800

    feat: basic layout for broadcast gui

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-6xl text-red-500"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-[1920px] h-[1080px] mx-auto text-white flex flex-col items-stretch p-4">
+      <div className="flex flex-row items-start">
+        <div>東一局</div>
+        <div className="flex-1" />
+        <div>LOGO</div>
+      </div>
+      <div className="flex-1" />
+      <div className="flex flex-row items-end justify-center gap-x-4">
+        <div>A</div>
+        <div>B</div>
+        <div>C</div>
+        <div>D</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
