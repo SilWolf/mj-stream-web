f1c154d83f33e7722317976533389fefa7fee6c5
SilWolf <silwolf1121@gmail.com>
2023-01-12 17:49:55 +0800

    feat: component - MJMatchRoundAndSubSpan

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import MJMatchRoundAndSubSpan from './components/MJMatchRoundAndSubSpan'
 
 function App() {
   return (
     <div className="w-[1920px] h-[1080px] mx-auto text-white flex flex-col items-stretch p-4">
       <div className="flex flex-row items-start">
-        <div>東一局</div>
+        <div>
+          <MJMatchRoundAndSubSpan>1.1</MJMatchRoundAndSubSpan>
+        </div>
         <div className="flex-1" />
         <div>LOGO</div>
       </div>
